cb3076918e3c5229c536af0e600f4ecfce577235
batako-san <hkawabata_work@yahoo.co.jp>
2016-08-27 09:00:15 +0900

    use log4j

--- src/test/java/jp/hkawabata/math/util/integral/IntegralTest.java
+++ src/test/java/jp/hkawabata/math/util/integral/IntegralTest.java
@@ -3,11 +3,14 @@ package jp.hkawabata.math.util.integral;
 import static junit.framework.Assert.*;
 
 import jp.hkawabata.math.util.func.NOrderFunction;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
 
 /**
  * 積分のテスト
@@ -15,6 +18,9 @@ import java.util.List;
  * Created by kawabatahiroto on 2016/08/26.
  */
 public class IntegralTest {
+
+    //Logger logger = Logger.getLogger(getClass().getName());
+    Logger logger = Logger.getLogger(getClass().getName());
 
     double delta = 0.1;
     private List<Integral> integrals = new ArrayList<>();
@@ -31,9 +37,10 @@ public class IntegralTest {
         double indices[] = {2, 1};
         NOrderFunction function = new NOrderFunction(n, indices);
 
+        logger.info(String.format("f(x) = %.2f x + %.2f", indices[0], indices[1]));
         for (Integral integral: integrals) {
             double result = integral.calculate(function, 0, 3);
-            System.out.println(result);
+            logger.info(String.format("\t%s: %f", integral.getClass().getSimpleName(), result));
             assertEquals(12, result, delta);
         }
     }
@@ -44,9 +51,10 @@ public class IntegralTest {
         double indices[] = {3, 2, 1};
         NOrderFunction function = new NOrderFunction(n, indices);
 
+        logger.info(String.format("f(x) = %.2f x^2 + %.2f x + %.2f", indices[0], indices[1], indices[2]));
         for (Integral integral: integrals) {
             double result = integral.calculate(function, 0, 3);
-            System.out.println(result);
+            logger.info(String.format("\t%s: %f", integral.getClass().getSimpleName(), result));
             assertEquals(39, result, delta);
         }
     }
